40f4da9c1a7bd2079f67cada38216fd76064f135
Maikel Contramaestre <116273585+BlackNereus@users.noreply.github.com>
2025-02-12 20:32:26 +0000

    Table-models-starWars

--- src/models.py
+++ src/models.py
@@ -7,26 +7,57 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    people_id = Column(Integer, ForeignKey('peoples.id'), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planets.id'), nullable=True)
+    species_id = Column(Integer, ForeignKey('species.id'), nullable=True)
+    
+    user = relationship('Users', foreign_keys=[user_id] , backref='favorites')
+    people = relationship('Peoples', foreign_keys=[people_id] , backref='favorites')
+    planet = relationship('Planets',foreign_keys=[planet_id] , backref='favorites')
+    species = relationship('Species',foreign_keys=[species_id], backref='favorites')
+
+
+class Users(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column (String(250), unique=True, nullable=False)
+    password = Column (String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Peoples(Base):
+    __tablename__ = 'peoples'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(80), nullable=False)
+    hair_color = Column(String(80), nullable=False)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship(Users, backref='peoples')
 
-    def to_dict(self):
-        return {}
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    terrain = Column(String(80), nullable=False)
+    climate = Column(String(80), nullable=False)
+    rotation_period  = Column(Integer, nullable=False)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship(Users, backref='planets')
+
+class Species(Base):
+    __tablename__ = 'species'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    average_height = Column(Integer, nullable=False)
+    hair_colors = Column(String(80), nullable=False)
+    skin_colors= Column(String(80), nullable=False)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship(Users, backref='species')
+    
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
